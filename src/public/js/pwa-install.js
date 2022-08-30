94b73510d9b445e040b1d07f7041222a7caccf9b
jcbbb <avaz28082000@gmail.com>
2022-08-30 11:25:55 +0500

    feat: added custom pwa install button;
    - Hide QR-code on mobile;

---
+++ src/public/js/pwa-install.js
@@ -0,0 +1,16 @@
+const install_btn = document.querySelector(".js-install-btn");
+
+let deferred_prompt = null;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferred_prompt = e;
+  install_btn.classList.add("scale-100");
+});
+
+window.addEventListener("appinstalled", (e) => {
+  install_btn.classList.remove("scale-100");
+  deferred_prompt = null;
+});
+
+install_btn.addEventListener("click", () => deferred_prompt.prompt());
