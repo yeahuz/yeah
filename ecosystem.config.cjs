58307bfa761ad429883d3f942ed9140cd1f8fb3f
jcbbb <avaz28082000@gmail.com>
2022-11-17 15:14:01 +0500

    fix: run processes without npm;

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,14 @@ module.exports = {
   apps: [
     {
       name: "needs",
-      script: "npm",
-      args: "start",
+      script: "./src/index.js",
       instances: 2,
       exec_mode: "cluster",
       restart_delay: 10000,
       listen_timeout: 10000,
-      env_production: {
+      env: {
         NODE_ENV: "production",
-      },
+      }
     },
   ],
 };
