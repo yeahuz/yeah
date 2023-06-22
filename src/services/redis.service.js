cca30a8aa4c2f1ffa7811ef3d30a21dc12730d72
jcbbb <avaz28082000@gmail.com>
2023-06-22 16:50:39 +0500

    refactor: move text messages to pub/sub;

--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -1,11 +1,36 @@
 import Redis from 'ioredis'
+import * as ChatService from "./chat.service.js";
+import { option } from "../utils/index.js";
 
 export const redis_client = new Redis()
 export const sub = new Redis();
 
-sub.on("message", (channel, message) => {
-  console.log({ channel, message })
+sub.on("message", async (channel, message) => {
+  switch (channel) {
+    case "api/messages": {
+      const result = JSON.parse(message);
+      let item = await redis_client.rpop(result.queue)
+      if (!item) return
+      item = JSON.parse(item)
+      const [msg, err] = await option(ChatService.create_message({
+        chat_id: item.chat_id,
+        content: item.content,
+        sender_id: item.sender_id,
+        reply_to: item.reply_to
+      }));
+
+      if (err) {
+        redis_client.rpush(result.queue, message)
+        return
+      }
+
+      redis_client.publish("messages/sent", JSON.stringify(Object.assign(msg, { temp_id: item.temp_id })))
+    } break;
+    default:
+      break
+  }
 })
 
 sub.subscribe("api")
+sub.subscribe("api/messages")
 
